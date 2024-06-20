a8e3478f986c640c128921bc116bcd1ec3620894
Latharagav <tgpremlatha@gmail.com>
2024-06-20 00:03:15 -0500

    Strings

--- stringing-characters-together/code-snippets/bracket-notation.js
+++ stringing-characters-together/code-snippets/bracket-notation.js
@@ -2,3 +2,40 @@ let jsCreator = "Brendan Eich";
 
 console.log(jsCreator[-1]);
 console.log(jsCreator[42]);
+//phrase = 'Code for fun';
+//console.log(phrase[2]);
+
+let phrase = "JavaScript rocks!";
+console.log(phrase[phrase.length - 8]);
+
+cityName = "Vienna";
+stateName = "Virginia";
+location = cityName + ", " +  stateName;
+console.log(cityName.length);
+console.log(stateName.length);
+
+console.log(location.length);
+
+pet = 'cat';
+console.log(pet + 's');
+console.log(pet += 's');
+
+
+let org = "  The LaunchCode Foundation ";
+let trimmed = org.trim();
+
+console.log(trimmed);
+
+let nonprofit = "LaunchCode";
+
+console.log(nonprofit.charCodeAt(0));
+console.log(nonprofit.charCodeAt(1));
+console.log(nonprofit.charCodeAt(2));
+console.log(nonprofit.charCodeAt(3));
+console.log(nonprofit.charCodeAt(4));
+console.log(nonprofit.charCodeAt(5));
+console.log(nonprofit.charCodeAt(6));
+console.log(nonprofit.charCodeAt(7));
+console.log(nonprofit.charCodeAt(8));
+console.log(nonprofit.charCodeAt(9));
+console.log("A message\nbroken across lines,\n\tand indented");
--- stringing-characters-together/code-snippets/method-chaining.js
+++ stringing-characters-together/code-snippets/method-chaining.js
@@ -6,6 +6,21 @@ console.log(word.toUpperCase());
 //Returns ``JAVASCRIPT``
 
 //What does ``word.slice(4).toUpperCase()`` return?
-
+word = 'JavaScript';
+console.log(word.slice(4).toUpperCase());
 
 //Experiment with other combinations (chains) of string methods.
+let language = "JavaScript";
+
+console.log(language.replace('J', 'Q'));
+console.log("After replacing J by Q " + language);
+language.slice(0,5);
+console.log(language);
+language = language.slice(1,6);
+console.log(language);
+
+console.log('Launch\nCode');
+let name = "Jack";
+let currentAge = 9;
+
+console.log(`Next year, ${name} will be ${currentAge + 1}.`);
