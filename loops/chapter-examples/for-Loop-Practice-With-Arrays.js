7659e314d24b391f9c6b6226ce4c1075ff07d840
Latharagav <tgpremlatha@gmail.com>
2024-06-22 01:07:12 -0500

    For-Loop-Chapter-Examples

--- loops/chapter-examples/for-Loop-Practice-With-Arrays.js
+++ loops/chapter-examples/for-Loop-Practice-With-Arrays.js
@@ -1,3 +1,7 @@
 // create an array variable containing the names
+let familyNames = ['Ragav', 'Premalatha', 'Eshaan', 'Riyaan'];
 
 // write a for loop that prints each name on a different line
+for( let i = 0; i < familyNames.length; i++){
+    console.log(familyNames[i]);
+}
--- loops/chapter-examples/while-Loop-Example.js
+++ loops/chapter-examples/while-Loop-Example.js
@@ -1,6 +1,16 @@
 let i = 0;
 
-while (i < 51) {
+while (i < 2) {
   console.log(i);
   i++;
+}
+
+const input = require('readline-sync');
+
+let num = input.question('Please enter a positive number:');
+num = Number(num);
+
+while (num <= 0) {
+   num = input.question('Invalid input. Please enter a positive number:');
+   num = Number(num);
 }
