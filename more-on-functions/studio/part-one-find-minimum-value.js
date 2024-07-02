318c833d315896db65489adb760677322fa16576
Latharagav <tgpremlatha@gmail.com>
2024-07-01 23:50:10 -0500

    Studio - More on Function

--- more-on-functions/studio/part-one-find-minimum-value.js
+++ more-on-functions/studio/part-one-find-minimum-value.js
@@ -18,8 +18,10 @@ let nums1 = [5, 10, 2, 42];
 let nums2 = [-2, 0, -10, -44, 5, 3, 0, 3];
 let nums3 = [200, 5, 4, 10, 8, 5, -3.3, 4.4, 0];
 
-console.log(arrayToBeSorted(nums3));
 
 //Using one of the test arrays as the argument, call your function inside the console.log statement below.
 
-console.log(/* your code here */);
+console.log(arrayToBeSorted(nums1));
+console.log(arrayToBeSorted(nums2));
+console.log(arrayToBeSorted(nums3));
+
