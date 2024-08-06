04e13838f2fcc4664e28b2737d6e3d34107a316d
Latharagav <tgpremlatha@gmail.com>
2024-08-05 22:29:53 -0500

    DOM and Events

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -4,7 +4,24 @@ function init () {
     const paragraph = document.getElementById("statusReport");
 
     // Put your code for the exercises here.
-    
+    button.addEventListener('click',event => {
+        paragraph.innerHTML = 'Houston! We have liftoff!';
+    });
+
+    missionAbort.addEventListener('mouseover', function(event) {
+        missionAbort.style.backgroundColor = 'red';
+    });
+
+    missionAbort.addEventListener('mouseout', function(event) {
+        missionAbort.style.backgroundColor = '';
+    });
+
+    missionAbort.addEventListener('click', event => {
+       let response = window.confirm("Are you sure you want to abort the mission?");
+       if(response){
+        paragraph.innerHTML = 'Mission aborted! Space shuttle returning home';
+       }
+    });
 }
 
 window.addEventListener("load", init);
